b3acfc00d55485c0a11846679f8c69c8021e2400
pyonggon <pyonggon11@naver.com>
2018-03-27 15:58:47 +0900

    디비 연동

--- chart_viewer/models.py
+++ chart_viewer/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.db.models.base import ModelBase
 
-# Create your models here.
+def get_chart_data():
+    query_string = """
+    select Floor((result.row-1) / 60) + 1 as id
+        , max(case when mod((result.row-1), 60) = 0 then date end) as date
+        , max(price) as high
+        , min(price) as low 
+        , max(case when mod((result.row-1), 60) = 0 then price end) as open
+        , max(case when mod((result.row-1), 60) = 59 then price end) as close
+          from (select @rownum:=@rownum+1 as row
+                      , price , cast(date as unsigned) as date
+                 from GCJ17 
+                  , (select @rownum:=0) R
+               )result
+                 group by Floor((result.row-1) / 60)
+    """
+
+    from django.db import connection
+
+    cursor = connection.cursor()
+    res = cursor.execute(query_string)
+    result = dictfetchall(cursor)
+    return result
+
+def get_deposit_history():
+    query_string = "select * from deposit_history"
+
+    from django.db import connection
+
+    cursor = connection.cursor()
+    res = cursor.execute(query_string)
+    result = dictfetchall(cursor)
+    return result
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+def create_model(db_table):
+    print("db_table: " + db_table)
+
+    class CustomMetaClass(ModelBase):
+        def __new__(cls, name, bases, attrs):
+            model = super(CustomMetaClass, cls).__new__(cls, name, bases, attrs)
+            model._meta.db_table = db_table
+            return model
+
+    class CustomModel(models.Model, metaclass=CustomMetaClass):
+
+        # define your fileds here
+        date = models.CharField(max_length=14)
+        open = models.CharField(max_length=9)
+        high = models.CharField(max_length=9)
+        low = models.CharField(max_length=9)
+        close = models.CharField(max_length=9)
+    return CustomModel
--- chart_viewer/views.py
+++ chart_viewer/views.py
@@ -4,6 +4,9 @@ import pymysql
 
 
 # Create your views here.
+from chart_viewer.models import get_chart_data
+
+
 def index(request):
 
     return render(request, 'chart_viewer/index.html', {})
@@ -11,3 +14,18 @@ def index(request):
 
 def chart(request):
     return render(request, 'chart_viewer/charts/viewer.html', {})
+
+
+def get_chart(request):
+    result = get_chart_data()
+
+    # for i in range(0, len(result)):
+    #     result[i]['date'] = str(result[i]['date'])
+    #     result[i]['date'] = result[i]['date'][0:4] + result[i]['date'][4:6] + result[i]['date'][6:12]
+    #     result[i].pop('id', None)
+
+    for i in range(0, len(result)):
+        temp_result = result[i]
+        result[i] = [temp_result['date'], temp_result['open'], temp_result['high'], temp_result['low'], temp_result['close']]
+
+    return render(request, 'chart_viewer/charts/viewer.html', {'data': result[:1000]})
