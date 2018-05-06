66b66644d2d8997e9347012521f197149dcee4dd
pyonggon <pyonggon11@naver.com>
2018-05-06 20:40:03 +0900

    차트 수정

--- haedong/settings.py
+++ haedong/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'x5gfq0@b4=94-q83%mzl=0b1#h=fst@fbvh)q^zfuu7voym^@*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
